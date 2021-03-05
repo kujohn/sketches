921f1fd04f6842a3e62b2538e5bfb51f6316c88e
John Ku <hellojohnku@gmail.com>
2021-03-04 17:18:41 -0800

    wip

--- bouncer/sketch.js
+++ bouncer/sketch.js
@@ -1,5 +1,5 @@
-const w = 1000;
-const h = 800;
+const w = 900;
+const h = 700;
 const center = new p5.Vector(w/2, h/2)
 
 const fps = 60;
@@ -11,26 +11,36 @@ let domCaptureFrames
 
 let captureStart = 0;
 let captureEnd = 0;
-let captureDuration = 1;
 let captureBuffer = null;
 let capturing = false;
+
+let captureImmediately = false;
+let captureDuration = 100;
 
 function setup() {
   createCanvas(w, h);
   frameRate(fps);
   domFrames = select("#frames")
+  domCaptureFrames = select("#duration")
+  domCaptureFrames.elt.value = captureDuration
   domCapture = select("#capture")
-  domCaptureFrames = select("#userFrames")
   domCapture.elt.addEventListener('click', () => {
     captureBuffer = createGraphics(w, h, SVG);
     captureDuration = parseInt(domCaptureFrames.elt.value);
     captureStart = frameCount + 1;
     captureEnd = frameCount + 1 + captureDuration;
   });
+
   createBalls();
 }
 
 function draw() {
+  // start capture automatically
+  if (captureImmediately) {
+    domCapture.elt.click();
+    captureImmediately = false;
+  }
+
   // update capturing
   capturing = frameCount >= captureStart && frameCount < captureEnd;
   if (capturing) {
@@ -40,7 +50,7 @@ function draw() {
   // update frame count in dom
   domFrames.elt.innerHTML = frameCount
 
-  // actual drawing;
+  // actual drawing
   drawer();
 
   // save
@@ -55,7 +65,7 @@ function draw() {
 /************ START DRAWING ************/
 /***************************************/
 
-let total = 1;
+let total = 5;
 let balls = []
 
 function drawer() {
@@ -69,7 +79,7 @@ function drawer() {
 
     // apply gravity
     const g = createVector(0, 0.4)
-    ball.applyForce(g)
+    // ball.applyForce(g)
     ball.update();
     ball.draw()
   }
--- template-p5svg/sketch.js
+++ template-p5svg/sketch.js
@@ -1,5 +1,5 @@
-const w = 1000;
-const h = 800;
+const w = 900;
+const h = 700;
 const center = new p5.Vector(w/2, h/2)
 
 const fps = 60;
@@ -11,28 +11,37 @@ let domCaptureFrames
 
 let captureStart = 0;
 let captureEnd = 0;
-let captureDuration = 1;
-let capturing = false;
 let captureBuffer = null;
+let capturing = false;
+
+let captureImmediately = false;
+let captureDuration = 100;
 
 function setup() {
   createCanvas(w, h);
   frameRate(fps);
   domFrames = select("#frames")
+  domCaptureFrames = select("#duration")
+  domCaptureFrames.elt.value = captureDuration
   domCapture = select("#capture")
-  domCaptureFrames = select("#userFrames")
   domCapture.elt.addEventListener('click', () => {
     captureBuffer = createGraphics(w, h, SVG);
     captureDuration = parseInt(domCaptureFrames.elt.value);
     captureStart = frameCount + 1;
     captureEnd = frameCount + 1 + captureDuration;
   });
+  init();
 }
 
 function draw() {
+  // start capture automatically
+  if (captureImmediately) {
+    domCapture.elt.click();
+    captureImmediately = false;
+  }
+
   // update capturing
   capturing = frameCount >= captureStart && frameCount < captureEnd;
-
   if (capturing) {
     domCapture.elt.innerHTML = captureEnd - frameCount;
   }
@@ -40,7 +49,7 @@ function draw() {
   // update frame count in dom
   domFrames.elt.innerHTML = frameCount
 
-  // actual drawing;
+  // actual drawing
   drawer();
 
   // save
@@ -54,6 +63,9 @@ function draw() {
 /***************************************/
 /************ START DRAWING ************/
 /***************************************/
+
+function init() {
+}
 
 function drawer() {
 }
